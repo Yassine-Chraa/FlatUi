4de16e5a6a4d7164ad6f5fe347c0bfc6b45bb018
Yassine-Chraa <yassinechraa1@gmail.com>
2023-03-27 03:24:51 +0000

    Add project files.

---
+++ FlatUi/core/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FlatUi.core
+{
+    internal class RelayCommand : ICommand
+    {
+        private Action<object> _execute;
+        private Func<object,bool> _canExecute;
+
+        public event EventHandler cancelExecuteChanged
+        {
+                add { CommandManager.RequerySuggested += value; }
+                remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action <object> execute, Func<object,bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
